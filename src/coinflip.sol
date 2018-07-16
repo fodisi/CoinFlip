e86db95c73b49867d59424e3923ae01f60584959
fodisi <fodisi@users.noreply.github.com>
2018-07-16 18:16:32 -0400

    Updated

--- src/coinflip.sol
+++ src/coinflip.sol
@@ -12,65 +12,66 @@ contract CoinFlip {
     // Defines the betting options. Head==0; Tail==1.
     enum BetOption {HEAD, TAIL}
 
+    event BetSessionOpened(uint sessionId, uint minimumBet, uint duration, uint openTimestamp);
     // Event to be raised when a new bet is placed.
-    event NewBet(uint betSessionId, address player, uint amount, uint option);
+    event NewBetPlaced(uint betSessionId, address player, uint amount, BetOption option);
     // Event to be raised when a new Result is announced for a bet session.
-    event NewResult(uint betSessionId, uint totalBetsCount, uint headBetsCount, uint tailBetsCount, BetOption betSessionResult);
+    event SessionResultAnnounced(
+        uint betSessionId,
+        uint totalBetsCount, 
+        uint headBetsCount,
+        uint tailBetsCount,
+        BetOption betSessionResult
+    );
     
-    // Represents a single bet.
+    // Represents a player's bet.
     struct Bet {
         address player;
         uint amount;
         BetOption option;
     }
+
+    // Represents a bet session, where players can place bets following the session constraints.
+    struct BetSession {
+        uint minimumBet;
+        uint ownerFee;
+        uint duration;
+        uint openTimestamp;
+        uint count;
+        uint headsCount;
+        uint tailsCount;
+        uint headsAmount;
+        uint tailsAmount;
+        //Bet[] placedBets;
+    }
     
-    // The result of a bet.
-    // BetOption public betResult; // 0 == tails; 1 == heads
-    // Determines the minimum value accepted for a bet (in wei - simplicity).
-    uint public minBet;
     // The contract's instance owner.
     address private owner;
-    uint private ownerFee;
-    // A key/value list of player and its bet.
-    Bet[] private bets;
-    // Stores the addresses of players who placed bets on HEADS.
-    //address[] private headPlayers;
-    // Stores the addresses of players who placed bets on TAILS.
-    //address[] private tailPlayers;
     // Unique identifier for a bet session.
-    uint private betSessionId = 0;
-    // Determines the date/time a bet was open.
-    uint private betOpenTimestamp;
-    // Determines the duration a of bet.
-    uint private betDuration;
-    // Counts the total number of bets placed by players.
-    uint private totalBetsCount;
-    // Counts the number of HEAD bets placed by players.
-    uint private headBetsCount;
-    // Counts the number of TAIL bets placed by players.
-    uint private tailBetsCount;
-    // $ amount placed on HEAD bets.
-    uint private headBetsAmount;
-    // $ amount placed on TAIL bets.
-    uint private tailBetsAmount;
+    uint private sessionIndex;
+    //BetSession currentSession;
+    BetSession[] private sessions;
+    // Maps the bets placed in each session.
+    mapping(uint => Bet[]) betsBySession;
+    // Indicates if there's an ongoing session.
+    bool ongoingSession = false;
     
     modifier onlyOwner() {
-        require (msg.sender == owner);
+        require(msg.sender == owner);
         _;
     }
 
     modifier notOwner() {
-        require (msg.sender != owner);
+        require(msg.sender != owner);
+        _;
+    }
+    modifier openForBets() {
+        require(block.timestamp <= sessions[sessionIndex].openTimestamp + (sessions[sessionIndex].duration * 1 minutes));
         _;
     }
 
-    modifier betIsOpen() {
-        require(block.timestamp <= betOpenTimestamp + betDuration * 1 minutes);
-        _;
-    }
-
-    modifier betIsClosed() {
-        require(block.timestamp > betOpenTimestamp + betDuration * 1 minutes);
+    modifier closedForBets() {
+        require(block.timestamp > sessions[sessionIndex].openTimestamp + (sessions[sessionIndex].duration * 1 minutes));
         _;
     }    
     
@@ -78,85 +79,99 @@ contract CoinFlip {
         owner = msg.sender;
     }
     
+    /** @dev Opens a session for bets.
+        @param minAmout the minimum amount to be allowed when placing bets.
+        @param duration the timeframe duration that the session will be open for bets.
+        @param fee the house fee that will be paid to the contract's owner.
+    */
     function openBetSession(uint minAmount, uint duration, uint fee) external onlyOwner {
         require(duration > 0);
         require(minAmount > 0);
+        require(fee > 0 && fee < 15); // house fee must be between 0 and 15%.
+        require(ongoingSession == false);
 
+        // Do not allow concurrent betting sessions.
+        ongoingSession = true;
+        uint openedAt = block.timestamp;
+        // Creates a new betting session using the specified parameters.
+        sessions.push(BetSession(minAmount, fee, duration, openedAt, 0, 0, 0, 0, 0));
         // Sets a new unique identifier for the bet session.
-        betSessionId++;
-        // Reset list of bets and players.
-        delete bets;
-        //delete headPlayers;
-        //delete tailPlayers;
-        // Reset bet counters.
-        totalBetsCount = 0;
-        headBetsCount = 0;
-        tailBetsCount = 0;
-        headBetsAmount = 0;
-        tailBetsAmount = 0;
-        // Sets bet properties.
-        minBet = minAmount;
-        betDuration = duration;
-        ownerFee = fee;
-        betOpenTimestamp = block.timestamp;
-
-        // TODO: raise an event when an bet is open.
+        sessionIndex = sessions.length - 1;
+        // Raises an event notifying a new betting session was open.
+        emit BetSessionOpened(sessionIndex, minAmount, duration, openedAt);
     }
     
-    /** @dev Allows a player to place a bet.
+    /** @dev Allows a player to place a bet on a specific outcome (head or tail).
         @param option Bet option chosen by the player. Allowed values are 0 (Heads) and 1 (Tails).
     */
-    function placePlayerBet(uint option) external payable notOwner betIsOpen {
+    function placePlayerBet(uint option) external payable notOwner openForBets {
+    // Used the header bellow to test on REMIX, since it was not allowing to execute the code
+    // from other accounts that were not the owner (function "At address").
+    //function placePlayerBet(uint option, address player) external payable openForBets {
         // Checks if player's bet value meets minimum bet requirement.
-        require(msg.value >= minBet);
+        require(msg.value >= sessions[sessionIndex].minimumBet);
         // Checks if player's option is a valid bet option. Value must be in (0==heads; 1==tails).
         require(option <= uint(BetOption.TAIL));
 
-        // Increments bet counters (total and specific option (head/tail)).
-        totalBetsCount++;
-        if (option == uint(BetOption.HEAD)) {
-            headBetsCount++;
-            headBetsAmount += msg.value;
-            //headPlayers.push(msg.sender);
-        } else {
-            tailBetsCount++;
-            tailBetsAmount += msg.value;
-            //tailPlayers.push(msg.sender);
-        }
-
         // Creates a new Bet and assigns it to the list of bets.
-        bets.push(Bet(msg.sender, msg.value, BetOption(option)));
+        betsBySession[sessionIndex].push(Bet(msg.sender, msg.value, BetOption(option)));
+        //betsBySession[sessionIndex].push(Bet(player, msg.value, BetOption(option)));
+        updateSessionStats(BetOption(option), msg.value);
 
         // Raises an event for the bet placed by the player.
-        emit NewBet(betSessionId, msg.sender, msg.value, option);
+        emit NewBetPlaced(sessionIndex, msg.sender, msg.value, BetOption(option));
     }
 
-    function announcesSessionResultAndPay() external onlyOwner betIsClosed {
+    /** @dev Announces the winning result for the betting session and pays out winners. */
+    function announcesSessionResultAndPay() external onlyOwner closedForBets {
         BetOption result = flipCoin();
         rewardWinners(result);
-        emit NewResult(betSessionId, totalBetsCount, headBetsCount, tailBetsCount, result);
+        ongoingSession = false;
+        emit SessionResultAnnounced(
+            sessionIndex,
+            sessions[sessionIndex].count,
+            sessions[sessionIndex].headsCount,
+            sessions[sessionIndex].tailsCount,
+            result
+        );
     }
     
-    function flipCoin() private view onlyOwner betIsClosed returns (BetOption) {
-        return BetOption(uint(keccak256(abi.encodePacked(block.timestamp, betSessionId))) % 2);
+    /** @dev Updates the stats (counters and amouts) of the current betting session.
+        @param option 
+    */
+    function updateSessionStats(BetOption option, uint betAmount) private openForBets {
+        // Increments bet counters (total and specific option (head/tail)).
+        sessions[sessionIndex].count++;
+        if (option == BetOption.HEAD) {
+            sessions[sessionIndex].headsCount++;
+            sessions[sessionIndex].headsAmount += betAmount;
+        } else {
+            sessions[sessionIndex].tailsCount++;
+            sessions[sessionIndex].tailsAmount += betAmount;
+        }
+    }
+
+    function flipCoin() private view onlyOwner closedForBets returns (BetOption) {
+        // PS: Known insecure random generation (designed for simplicity).
+        return BetOption(uint(keccak256(abi.encodePacked(block.timestamp, sessionIndex))) % 2);
     }
     
-    function rewardWinners(BetOption result) private onlyOwner betIsClosed {
+    function rewardWinners(BetOption result) private onlyOwner closedForBets {
         BetOption winningOption = BetOption(result);
         // calculates the fee that goes to the house/contract.
-        uint fee = address(this).balance * ownerFee / 100;
+        uint fee = address(this).balance * sessions[sessionIndex].ownerFee / 100;
         uint totalPrize = address(this).balance - fee;
         uint winningBetAmout = 0;
 
         if (winningOption == BetOption.HEAD) {
-            winningBetAmout = headBetsAmount;
+            winningBetAmout = sessions[sessionIndex].headsAmount;
         } else {
-            winningBetAmout = tailBetsAmount;
+            winningBetAmout = sessions[sessionIndex].tailsAmount;
         }
 
         // Pays out players
-        for (uint i = 0; i < bets.length; i++) {
-            Bet memory curBet = bets[i];
+        for (uint i = 0; i < betsBySession[sessionIndex].length; i++) {
+            Bet memory curBet = betsBySession[sessionIndex][i];
             if (curBet.option == winningOption) {
                 // Gets the percentage of the player's bet, em relation to the amount
                 // betted on the winning result.
@@ -172,7 +187,6 @@ contract CoinFlip {
 
         // Pays owner's fee - gas.
         owner.transfer(address(this).balance);
-
     }
 
     // IMPROVEMENT IDEA - Create public function that can be called after a period of time by any
